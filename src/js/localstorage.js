56881e784dbf35ad2ce61c66a56b2f7e39b399ce
Julia-Davydiuk <70822407+Julia-Davydiuk@users.noreply.github.com>
2021-03-07 11:28:07 +0100

    доделала

--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -4,4 +4,27 @@ const Theme = {
   };
 
   const bodyRef = document.querySelector('body');
-  console.log(bodyRef)
+  const checkboxRef = document.querySelector('#theme-switch-toggle');
+
+  checkboxRef.addEventListener('change', toggleCheckbox);
+
+function toggleCheckbox() {
+  if (checkboxRef.checked) {
+    bodyRef.classList.remove(Theme.LIGHT);
+    bodyRef.classList.add(Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+  } else {
+    bodyRef.classList.remove(Theme.DARK);
+    bodyRef.classList.add(Theme.LIGHT);
+    localStorage.setItem('theme', Theme.LIGHT);
+  }
+}
+
+ renewalTheme();
+
+function renewalTheme() {
+  if (localStorage.getItem('theme') === Theme.DARK) {
+    bodyRef.classList.add(Theme.DARK);
+    checkboxRef.checked = true;
+  }
+}
